772fcb21096b18ae82010c39072bb03e98f92d6a
Carlos Miguel Jenkins Perez <carlos.jenkins@hpe.com>
2016-07-27 17:12:41 -0600

    Added instructions to setup I2C environment on the chip.

--- semaphore/chip/setup.sh
+++ semaphore/chip/setup.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
+# Locales config
 sudo apt-get install locales
 sudo dpkg-reconfigure locales
 sudo locale-gen en_US.UTF-8
 sudo update-locale LANG=en_US.UTF-8
-sudo apt-get install python3 ca-certificates tree git
+
+# Basic tools
+sudo apt-get install ca-certificates tree git
+
+# Python environment
+sudo apt-get install python3
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python3 get-pip.py
 sudo pip3 install tox webdev
+
+# I2C
+sudo apt-get install build-essential i2c-tools libi2c-dev libffi-dev python3-dev
+sudo pip3 install smbus-cffi
+# sudo adduser chip i2c
+# Execute: sudo i2cdetect -y 0/2
+# Check: ls /dev/i2c*
