9ba2d757efd232519c9d9541089f6e3670fde9f6
Carolina Aguilar <caroagse@gmail.com>
2016-07-31 13:12:09 -0600

    Added base scripts for RF Link (Car Receiver and Semaphore Transmitter)

---
+++ car/arduino/firmware/RF_Receiver.ino
@@ -0,0 +1,26 @@
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
+
+RH_ASK driver;
+
+void setup()
+{
+    Serial.begin(9600);
+    if (!driver.init())
+         Serial.println("init failed");
+}
+
+char buf[64];
+
+void loop()
+{
+    uint8_t received = sizeof(buf);
+    if (driver.recv((uint8_t *)buf, &received)) // Non-blocking
+    {
+      buf[received] = '\0';
+      Serial.print("Message: (");
+      Serial.print(received);
+      Serial.print(") ");
+      Serial.println(buf);
+    }
+}
