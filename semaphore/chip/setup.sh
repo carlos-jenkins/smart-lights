4575ea74e9633745b226d7b2d86c285cf31323b4
Carlos Jenkins <carlos@jenkins.co.cr>
2016-07-31 22:40:55 -0600

    Improved CHIP support script and added instructions to get IP on CHIP.

--- semaphore/chip/setup.sh
+++ semaphore/chip/setup.sh
@@ -9,17 +9,22 @@ sudo locale-gen en_US.UTF-8
 sudo update-locale LANG=en_US.UTF-8
 
 # Basic tools
-sudo apt-get install ca-certificates tree git
+sudo apt-get install -y ca-certificates tree git
 
 # Python environment
-sudo apt-get install python3
+sudo apt-get install -y python3
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python3 get-pip.py
 sudo pip3 install tox webdev
 
 # I2C
-sudo apt-get install build-essential i2c-tools libi2c-dev libffi-dev python3-dev
+sudo apt-get install -y build-essential i2c-tools libi2c-dev libffi-dev python3-dev
 sudo pip3 install smbus-cffi
 # sudo adduser chip i2c
 # Execute: sudo i2cdetect -y 0/2
 # Check: ls /dev/i2c*
+
+# User configuration
+# Add to .bashrc:
+# alias la='ls -lah'
+# export PATH=/usr/sbin:$PATH
