7f7776a88e7623469878546408666030a551a962
Carlos Jenkins <carlos@jenkins.co.cr>
2016-08-02 15:13:16 -0600

    Refactor constants of the BME280.

--- semaphore/chip/drivers/python/grid_io/bme280.py
+++ semaphore/chip/drivers/python/grid_io/bme280.py
@@ -26,115 +26,132 @@
 # THE SOFTWARE.
 
 from time import sleep
+from argparse import Namespace
 
 from .i2c import I2CDevice
 
 
-# BME280 default address.
-BME280_I2CADDR = 0x77
-
-# Operating Modes
-BME280_OSAMPLE_1 = 1
-BME280_OSAMPLE_2 = 2
-BME280_OSAMPLE_4 = 3
-BME280_OSAMPLE_8 = 4
-BME280_OSAMPLE_16 = 5
-
-# BME280 Registers
-
-BME280_REGISTER_DIG_T1 = 0x88  # Trimming parameter registers
-BME280_REGISTER_DIG_T2 = 0x8A
-BME280_REGISTER_DIG_T3 = 0x8C
-
-BME280_REGISTER_DIG_P1 = 0x8E
-BME280_REGISTER_DIG_P2 = 0x90
-BME280_REGISTER_DIG_P3 = 0x92
-BME280_REGISTER_DIG_P4 = 0x94
-BME280_REGISTER_DIG_P5 = 0x96
-BME280_REGISTER_DIG_P6 = 0x98
-BME280_REGISTER_DIG_P7 = 0x9A
-BME280_REGISTER_DIG_P8 = 0x9C
-BME280_REGISTER_DIG_P9 = 0x9E
-
-BME280_REGISTER_DIG_H1 = 0xA1
-BME280_REGISTER_DIG_H2 = 0xE1
-BME280_REGISTER_DIG_H3 = 0xE3
-BME280_REGISTER_DIG_H4 = 0xE4
-BME280_REGISTER_DIG_H5 = 0xE5
-BME280_REGISTER_DIG_H6 = 0xE6
-BME280_REGISTER_DIG_H7 = 0xE7
-
-BME280_REGISTER_CHIPID = 0xD0
-BME280_REGISTER_VERSION = 0xD1
-BME280_REGISTER_SOFTRESET = 0xE0
-
-BME280_REGISTER_CONTROL_HUM = 0xF2
-BME280_REGISTER_CONTROL = 0xF4
-BME280_REGISTER_CONFIG = 0xF5
-BME280_REGISTER_PRESSURE_DATA = 0xF7
-BME280_REGISTER_TEMP_DATA = 0xFA
-BME280_REGISTER_HUMIDITY_DATA = 0xFD
-
-
 class BME280(I2CDevice):
+    """
+    Python I2C driver for the BME280 temperature, humidity and pressure sensor
+    by Bosch.
+
+    Tested with Adafruit breakout board:
+
+        https://www.adafruit.com/products/2652
+
+    By default, the I2C address is 0x77.
+    If you add a jumper from SDO to GND, the address will change to 0x76.
+    """
+
+    _registers = Namespace(**{
+        'DIG_T1': 0x88,  # Trimming parameter registers
+        'DIG_T2': 0x8A,
+        'DIG_T3': 0x8C,
+
+        'DIG_P1': 0x8E,
+        'DIG_P2': 0x90,
+        'DIG_P3': 0x92,
+        'DIG_P4': 0x94,
+        'DIG_P5': 0x96,
+        'DIG_P6': 0x98,
+        'DIG_P7': 0x9A,
+        'DIG_P8': 0x9C,
+        'DIG_P9': 0x9E,
+
+        'DIG_H1': 0xA1,
+        'DIG_H2': 0xE1,
+        'DIG_H3': 0xE3,
+        'DIG_H4': 0xE4,
+        'DIG_H5': 0xE5,
+        'DIG_H6': 0xE6,
+        'DIG_H7': 0xE7,
+
+        'CHIPID': 0xD0,
+        'VERSION': 0xD1,
+        'SOFTRESET': 0xE0,
+
+        'CONTROL_HUM': 0xF2,
+        'CONTROL': 0xF4,
+        'CONFIG': 0xF5,
+        'PRESSURE_DATA': 0xF7,
+        'TEMP_DATA': 0xFA,
+        'HUMIDITY_DATA': 0xFD
+    })
+
+    modes = Namespace(**{
+        'ULTRALOWPOWER': 1,       # Oversampling 1
+        'LOWPOWER': 2,            # Oversampling 2
+        'STANDARD': 3,            # Oversampling 4
+        'HIGHRESOLUTION': 4,      # Oversampling 8
+        'ULTRAHIGHRESOLUTION': 5  # Oversampling 16
+    })
+
     def __init__(
-            self, busnum, address,
-            mode=BME280_OSAMPLE_1,
+            self, busnum, address=0x77,
+            mode=modes.ULTRALOWPOWER,
             **kwargs):
 
         super().__init__(busnum, address)
 
         # Check that mode is valid.
-        if mode not in [
-            BME280_OSAMPLE_1,
-            BME280_OSAMPLE_2,
-            BME280_OSAMPLE_4,
-            BME280_OSAMPLE_8,
-            BME280_OSAMPLE_16
-        ]:
+        if mode not in vars(BME280.modes).values():
             raise ValueError(
                 'Unexpected mode value {0}. '
-                'Set mode to one of BME280_ULTRALOWPOWER, BME280_STANDARD, '
-                'BME280_HIGHRES, or BME280_ULTRAHIGHRES'.format(mode)
+                'Set mode to one of {}'.format(
+                    ', '.join([
+                        'BME280.modes.{}'.format(key)
+                        for key in BME280.modes.keys()
+                    ])
+                )
             )
         self._mode = mode
 
         # Load calibration values.
         self._load_calibration()
-        self.register_write_u8(BME280_REGISTER_CONTROL, 0x3F)
+
+        # Initial known setting: 0xF3 0b00111111
+        #
+        # Bit 7, 6, 5 osrs_t[2:0] Controls oversampling of temperature data.
+        #     001 -> oversampling ×1. See table 22 in datasheet.
+        # Bit 4, 3, 2 osrs_p[2:0] Controls oversampling of pressure data.
+        #     111 -> oversampling ×16. See table 22 in datasheet.
+        # Bit 1, 0 mode[1:0] Controls the power mode of the device.
+        #     11 -> Normal mode. See table 10.
+        self.register_write_u8(BME280._registers.CONTROL, 0x3F)
         self.t_fine = 0.0
 
     def _load_calibration(self):
 
-        self.dig_t1 = self.register_read_u16le(BME280_REGISTER_DIG_T1)
-        self.dig_t2 = self.register_read_s16le(BME280_REGISTER_DIG_T2)
-        self.dig_t3 = self.register_read_s16le(BME280_REGISTER_DIG_T3)
+        self._dig_t1 = self.register_read_u16le(BME280._registers.DIG_T1)
+        self._dig_t2 = self.register_read_s16le(BME280._registers.DIG_T2)
+        self._dig_t3 = self.register_read_s16le(BME280._registers.DIG_T3)
 
-        self.dig_p1 = self.register_read_u16le(BME280_REGISTER_DIG_P1)
-        self.dig_p2 = self.register_read_s16le(BME280_REGISTER_DIG_P2)
-        self.dig_p3 = self.register_read_s16le(BME280_REGISTER_DIG_P3)
-        self.dig_p4 = self.register_read_s16le(BME280_REGISTER_DIG_P4)
-        self.dig_p5 = self.register_read_s16le(BME280_REGISTER_DIG_P5)
-        self.dig_p6 = self.register_read_s16le(BME280_REGISTER_DIG_P6)
-        self.dig_p7 = self.register_read_s16le(BME280_REGISTER_DIG_P7)
-        self.dig_p8 = self.register_read_s16le(BME280_REGISTER_DIG_P8)
-        self.dig_p9 = self.register_read_s16le(BME280_REGISTER_DIG_P9)
+        self._dig_p1 = self.register_read_u16le(BME280._registers.DIG_P1)
+        self._dig_p2 = self.register_read_s16le(BME280._registers.DIG_P2)
+        self._dig_p3 = self.register_read_s16le(BME280._registers.DIG_P3)
+        self._dig_p4 = self.register_read_s16le(BME280._registers.DIG_P4)
+        self._dig_p5 = self.register_read_s16le(BME280._registers.DIG_P5)
+        self._dig_p6 = self.register_read_s16le(BME280._registers.DIG_P6)
+        self._dig_p7 = self.register_read_s16le(BME280._registers.DIG_P7)
+        self._dig_p8 = self.register_read_s16le(BME280._registers.DIG_P8)
+        self._dig_p9 = self.register_read_s16le(BME280._registers.DIG_P9)
 
-        self.dig_h1 = self.register_read_u8(BME280_REGISTER_DIG_H1)
-        self.dig_h2 = self.register_read_s16le(BME280_REGISTER_DIG_H2)
-        self.dig_h3 = self.register_read_u8(BME280_REGISTER_DIG_H3)
-        self.dig_h6 = self.register_read_s8(BME280_REGISTER_DIG_H7)
+        self._dig_h1 = self.register_read_u8(BME280._registers.DIG_H1)
+        self._dig_h2 = self.register_read_s16le(BME280._registers.DIG_H2)
+        self._dig_h3 = self.register_read_u8(BME280._registers.DIG_H3)
+        self._dig_h6 = self.register_read_s8(BME280._registers.DIG_H7)
 
-        h4 = self.register_read_s8(BME280_REGISTER_DIG_H4)
+        h4 = self.register_read_s8(BME280._registers.DIG_H4)
         h4 = (h4 << 24) >> 20
-        self.dig_h4 = h4 | (
-            self.register_read_u8(BME280_REGISTER_DIG_H5) & 0x0F
+        self._dig_h4 = h4 | (
+            self.register_read_u8(BME280._registers.DIG_H5) & 0x0F
         )
 
-        h5 = self.register_read_s8(BME280_REGISTER_DIG_H6)
+        h5 = self.register_read_s8(BME280._registers.DIG_H6)
         h5 = (h5 << 24) >> 20
-        self.dig_h5 = h5 | (
-            self.register_read_u8(BME280_REGISTER_DIG_H5) >> 4 & 0x0F
+        self._dig_h5 = h5 | (
+            self.register_read_u8(BME280._registers.DIG_H5) >> 4 & 0x0F
         )
 
     def _read_raw_temperature(self):
@@ -142,10 +159,10 @@ class BME280(I2CDevice):
         Reads the raw (uncompensated) temperature from the sensor.
         """
         meas = self._mode
-        self.register_write_u8(BME280_REGISTER_CONTROL_HUM, meas)
+        self.register_write_u8(BME280._registers.CONTROL_HUM, meas)
 
         meas = self._mode << 5 | self._mode << 2 | 1
-        self.register_write_u8(BME280_REGISTER_CONTROL, meas)
+        self.register_write_u8(BME280._registers.CONTROL, meas)
 
         # Wait the required time
         sleep_time = 0.00125 + 0.0023 * (1 << self._mode)
@@ -153,9 +170,9 @@ class BME280(I2CDevice):
         sleep_time = sleep_time + 0.0023 * (1 << self._mode) + 0.000575
         sleep(sleep_time)
 
-        msb = self.register_read_u8(BME280_REGISTER_TEMP_DATA)
-        lsb = self.register_read_u8(BME280_REGISTER_TEMP_DATA + 1)
-        xlsb = self.register_read_u8(BME280_REGISTER_TEMP_DATA + 2)
+        msb = self.register_read_u8(BME280._registers.TEMP_DATA)
+        lsb = self.register_read_u8(BME280._registers.TEMP_DATA + 1)
+        xlsb = self.register_read_u8(BME280._registers.TEMP_DATA + 2)
         raw = ((msb << 16) | (lsb << 8) | xlsb) >> 4
         return raw
 
@@ -166,9 +183,9 @@ class BME280(I2CDevice):
         Assumes that the temperature has already been read.
         Which means that enough delay has been provided.
         """
-        msb = self.register_read_u8(BME280_REGISTER_PRESSURE_DATA)
-        lsb = self.register_read_u8(BME280_REGISTER_PRESSURE_DATA + 1)
-        xlsb = self.register_read_u8(BME280_REGISTER_PRESSURE_DATA + 2)
+        msb = self.register_read_u8(BME280._registers.PRESSURE_DATA)
+        lsb = self.register_read_u8(BME280._registers.PRESSURE_DATA + 1)
+        xlsb = self.register_read_u8(BME280._registers.PRESSURE_DATA + 2)
         raw = ((msb << 16) | (lsb << 8) | xlsb) >> 4
         return raw
 
@@ -179,8 +196,8 @@ class BME280(I2CDevice):
         Assumes that the temperature has already been read.
         Which means that enough delay has been provided.
         """
-        msb = self.register_read_u8(BME280_REGISTER_HUMIDITY_DATA)
-        lsb = self.register_read_u8(BME280_REGISTER_HUMIDITY_DATA + 1)
+        msb = self.register_read_u8(BME280._registers.HUMIDITY_DATA)
+        lsb = self.register_read_u8(BME280._registers.HUMIDITY_DATA + 1)
         raw = (msb << 8) | lsb
         return raw
 
@@ -191,13 +208,13 @@ class BME280(I2CDevice):
         # float in Python is double precision
         uncompensated = float(self._read_raw_temperature())
         var1 = (
-            (uncompensated / 16384.0 - self.dig_t1 / 1024.0) *
-            float(self.dig_t2)
+            (uncompensated / 16384.0 - self._dig_t1 / 1024.0) *
+            float(self._dig_t2)
         )
         var2 = (
-            (uncompensated / 131072.0 - self.dig_t1 / 8192.0) *
-            (uncompensated / 131072.0 - self.dig_t1 / 8192.0) *
-            float(self.dig_t3)
+            (uncompensated / 131072.0 - self._dig_t1 / 8192.0) *
+            (uncompensated / 131072.0 - self._dig_t1 / 8192.0) *
+            float(self._dig_t3)
         )
         self.t_fine = int(var1 + var2)
         temp = (var1 + var2) / 5120.0
@@ -209,22 +226,22 @@ class BME280(I2CDevice):
         """
         adc = self._read_raw_pressure()
         var1 = self.t_fine / 2.0 - 64000.0
-        var2 = var1 * var1 * self.dig_p6 / 32768.0
-        var2 = var2 + var1 * self.dig_p5 * 2.0
-        var2 = var2 / 4.0 + self.dig_p4 * 65536.0
+        var2 = var1 * var1 * self._dig_p6 / 32768.0
+        var2 = var2 + var1 * self._dig_p5 * 2.0
+        var2 = var2 / 4.0 + self._dig_p4 * 65536.0
         var1 = (
-            self.dig_p3 * var1 * var1 / 524288.0 + self.dig_p2 * var1
+            self._dig_p3 * var1 * var1 / 524288.0 + self._dig_p2 * var1
         ) / 524288.0
-        var1 = (1.0 + var1 / 32768.0) * self.dig_p1
+        var1 = (1.0 + var1 / 32768.0) * self._dig_p1
 
         if var1 == 0:
             return 0
 
         p = 1048576.0 - adc
         p = ((p - var2 / 4096.0) * 6250.0) / var1
-        var1 = self.dig_p9 * p * p / 2147483648.0
-        var2 = p * self.dig_p8 / 32768.0
-        p = p + (var1 + var2 + self.dig_p7) / 16.0
+        var1 = self._dig_p9 * p * p / 2147483648.0
+        var2 = p * self._dig_p8 / 32768.0
+        p = p + (var1 + var2 + self._dig_p7) / 16.0
 
         return p
 
@@ -235,17 +252,17 @@ class BME280(I2CDevice):
         adc = self._read_raw_humidity()
         h = self.t_fine - 76800.0
         h = (
-            (adc - (self.dig_h4 * 64.0 + self.dig_h5 / 16384.8 * h)) *
+            (adc - (self._dig_h4 * 64.0 + self._dig_h5 / 16384.8 * h)) *
             (
-                self.dig_h2 / 65536.0 *
+                self._dig_h2 / 65536.0 *
                 (
-                    1.0 + self.dig_h6 / 67108864.0 * h * (
-                        1.0 + self.dig_h3 / 67108864.0 * h
+                    1.0 + self._dig_h6 / 67108864.0 * h * (
+                        1.0 + self._dig_h3 / 67108864.0 * h
                     )
                 )
             )
         )
-        h = h * (1.0 - self.dig_h1 * h / 524288.0)
+        h = h * (1.0 - self._dig_h1 * h / 524288.0)
 
         # Set boundary
         h = min(h, 100.0)
