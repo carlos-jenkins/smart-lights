b3448eed85e660344c73754265c1eaf962c557c7
Carolina Aguilar <caroagse@gmail.com>
2016-08-02 16:43:11 -0600

    Render Costa Rica map using Google Maps and markers coming from API.

--- cloud/web/src/App.js
+++ cloud/web/src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import { GoogleMapLoader, GoogleMap, InfoWindow, Marker } from 'react-google-maps';
+import { triggerEvent } from 'react-google-maps/lib/utils';
 
 import api from './helpers/grid_api';
 import './App.css';
 
 class App extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            // Center Google Map in Costa Rica
+            center: {
+              lat: 10.0416216,
+              lng: -84.1810092,
+            },
+            markers: []
+          }
+    }
+
     componentDidMount() {
         var self = this;
         api.getGrid(function(err, data) {
@@ -14,12 +28,36 @@ class App extends Component {
     render() {
         console.log(this.state)
         return (
-            <div className="App">
-                <div className="App-header">
-                    <h2>Welcome to Open Semaphore</h2>
-                </div>
+            <div className="container">
+                <h1>Open Semaphore</h1>
+                <GoogleMapLoader
+                    containerElement={ <div className="row map-container"/> }
+                    googleMapElement={
+                        <GoogleMap
+                            ref='map'
+                            center={this.state.center}
+                            defaultZoom={8}
+                            defaultOptions={{
+                                styles: require('./styledMap.json')
+                            }}>
+                            {this.state.markers.map((marker, index) => {
+                                const ref = `marker_${index}`;
+                                var latitude = marker.latitude;
+                                var longitude = marker.longitude;
+
+                                return (
+                                    <Marker
+                                        key={index}
+                                        ref={ref}
+                                        position={
+                                            new google.maps.LatLng(latitude, longitude)
+                                        }/>
+                                );
+                            })}
+                        </GoogleMap>
+                    }/>
             </div>
-          );
+        );
     }
 }
 
