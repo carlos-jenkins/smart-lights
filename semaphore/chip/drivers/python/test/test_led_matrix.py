92cb5603d286155e330970cf8249e2ff4c577d69
Carlos Jenkins <carlos@jenkins.co.cr>
2016-08-06 14:47:46 -0600

    Fixed bad bitmap test not waiting to show image.

--- semaphore/chip/drivers/python/test/test_led_matrix.py
+++ semaphore/chip/drivers/python/test/test_led_matrix.py
@@ -23,11 +23,8 @@ from grid_io.matrix8x8 import Matrix8x8
 def test_bitmap():
 
     m = Matrix8x8(2, 0x74)
-    sleep(4)
     m.clear()
     m.flush()
-    sleep(4)
-
     bitmap = [
         [0, 0, 0, 0, 1, 0, 0, 0],
         [0, 0, 0, 1, 1, 0, 0, 0],
@@ -39,6 +36,9 @@ def test_bitmap():
         [0, 0, 0, 1, 1, 1, 0, 0],
     ]
     m.write_bitmap(bitmap)
+    m.flush()
+    sleep(4)
+    m.clear()
     m.flush()
 
 
