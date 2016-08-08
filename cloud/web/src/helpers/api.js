6dea47dc46d972ca0fc0bc09cf4fed44ce39d309
Carolina Aguilar <caroagse@gmail.com>
2016-08-08 14:29:16 -0600

    Use fetch to make api requests.

--- cloud/web/src/helpers/api.js
+++ cloud/web/src/helpers/api.js
@@ -1,68 +1,51 @@
-import request from 'superagent';
-
-var baseUrl = 'http://hwthoncr16.herokuapp.com/thegrid';
-
-var api = {
-    get: function(url, callback) {
-        request
-           .get(baseUrl + url)
-           .set('Accept', 'application/json')
-           .end(function(err, res) {
-               callback(err, JSON.parse(res.text))
-           });
-    },
-
-    put: function(url, params, callback) {
-
-    },
-    post: function(url, params, callback) {
-
-    }
-};
-
-
+// var baseUrl = 'http://hwthoncr16.herokuapp.com/thegrid';
+var baseUrl = 'http://localhost:3001';
 
 module.exports = {
     getLast: function(callback) {
-        api.get('/last', function(err, result) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-
-            callback(result);
-        });
+        fetch(baseUrl + '/last')
+            .then(function(response) {
+                return response.json()
+            }).then(function(json) {
+                callback(json);
+            }).catch(function(ex) {
+                console.log('parsing failed', ex)
+                callback([]);
+            })
     },
 
     getAll: function(callback) {
-        api.get('/all', function(err, result) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-
-            callback(result);
-        });
+        fetch(baseUrl + '/all')
+            .then(function(response) {
+                return response.json()
+            }).then(function(json) {
+                callback(json);
+            }).catch(function(ex) {
+                console.log('parsing failed', ex)
+            })
     },
 
     getLastQty: function(params, callback) {
-        api.get('/all', function(err, result) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            var length = result.length;
-            callback(result.slice(length - params.qty, length));
-        });
+        fetch(baseUrl + '/all')
+            .then(function(response) {
+                return response.json()
+            }).then(function(json) {
+                var length = json.length;
+                callback(json.slice(length - params.qty, length));
+            }).catch(function(ex) {
+                console.log('parsing failed', ex)
+            })
     },
 
     setSemaphoreState: function(params, callback) {
-        var pythonServer = 'http://192.168.8.101:8080';
-        request
-           .get(pythonServer + '/state/' + params.state)
-           .set('Accept', 'application/json')
-           .end(function(err, res) {
-               callback && callback(err, JSON.parse(res.text))
+        var pythonServer = 'http://192.168.8.100:8080';
+        fetch(pythonServer + '/state/' + params.state)
+            .then(function(response) {
+               return response.json()
+            }).then(function(json) {
+               callback && callback(json);
+            }).catch(function(ex) {
+               console.log('parsing failed', ex)
            });
     }
 };
